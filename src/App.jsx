88a5b4bf2a2ec6713d9b05d47253db76293fb5d5
Marko Knöbl <marko.kn@gmail.com>
2023-02-11 13:12:31 +0100

    display appointments

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [appointments, setAppointments] = useState([
+    {
+      id: 1,
+      date: "2023-02-11",
+      time: "14:00",
+      name: "doctor",
+    },
+    {
+      id: 2,
+      date: "2023-02-11",
+      time: "15:00",
+      name: "shopping",
+    },
+  ]);
 
   return (
     <div className="App">
       <h1>Calendar</h1>
+      {appointments.map((appointment) => (
+        <li key={appointment.id}>
+          {appointment.date} {appointment.time} {appointment.name}
+        </li>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
